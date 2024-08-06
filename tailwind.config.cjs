bc8ac3374501863da836a579173f003eb2ddf4be
Saikat <saikat@Saikats-MacBook-Pro.local>
2024-08-07 00:31:20 +0530

    Adds vite react tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  prefix: "tw-",
+  content: ["./**/*/.liquid", "./frontend/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
